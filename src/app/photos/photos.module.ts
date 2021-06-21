1f5d2d733c30fe2d0d37004c248957a47c9854c2
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-06-21 09:01:00 -0300

    feat: create submodules

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,26 +1,26 @@
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
 import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
+import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
-  declarations: [
-    PhotoComponent,
-    PhotoListComponent,
-    PhotoFormComponent,
+  declarations: [ 
+    PhotoComponent, 
+    PhotoListComponent, 
+    PhotoFormComponent, 
     PhotosComponent,
     FilterByDescription,
     LoadButtonComponent
-  ],
-  imports: [
-    CommonModule,
-    HttpClientModule
-  ]
+],
+imports: [ 
+    HttpClientModule,
+    CommonModule 
+]
 })
 export class PhotosModule { }
