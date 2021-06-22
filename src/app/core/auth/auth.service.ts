a150776ede1203ce53c2b75b5e2def10b8397e55
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-06-22 17:52:33 -0300

    feat: token store

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,17 +1,29 @@
+import { TokenService } from './../token/token.service';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-
-const API_URL = 'http://localhost:3000';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService
+  ) { }
 
   authenticate(userName: string, password: string) {
-    return this.http.post(`${environment.baseUrl}/user/login`, { userName, password } )
+    return this.http
+    .post(
+      `${environment.baseUrl}/user/login`,
+      { userName, password },
+      { observe: 'response' }
+    )
+    .pipe(tap(res => {
+      const authToken = res.headers.get('x-access-token');
+      this.tokenService.setToken(authToken ?? '');
+    }))
   }
 }
