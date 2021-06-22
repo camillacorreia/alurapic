25700fb1d8799ced22ccd5ca9c4e9ec8063b409a
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-06-22 08:29:13 -0300

    feat: form validation

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     SignInComponent
+  ],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule
   ]
 })
 export class HomeModule { }
