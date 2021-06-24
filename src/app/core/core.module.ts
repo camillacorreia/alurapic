be880fc423c964c8df01ac600668e4a01762924d
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-06-24 08:25:59 -0300

    feat: interceptors and send token

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,8 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RequestInterceptor } from './auth/request.interceptor';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,13 @@ import { CommonModule } from '@angular/common';
   imports: [
     CommonModule,
     RouterModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptor,
+      multi: true
+    }
   ]
 })
 export class CoreModule { }
