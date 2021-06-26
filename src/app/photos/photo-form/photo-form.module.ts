359229abd049ce101ec171d658848895db8d69de
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-06-26 20:25:35 -0300

    feat: form upload

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,3 +1,5 @@
+import { MessageModule } from './../../shared/components/card/message/message.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PhotoFormComponent } from './photo-form.component';
@@ -7,7 +9,10 @@ import { PhotoFormComponent } from './photo-form.component';
     PhotoFormComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    MessageModule,
+    FormsModule
   ]
 })
 export class PhotoFormModule { }
